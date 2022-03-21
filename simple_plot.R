84cf4fa156cd1996aadda6b465869566a9228175
laurabiggins <laurabiggins10@gmail.com>
2022-03-21 15:15:29 +0000

    Changed colour to blue.

--- simple_plot.R
+++ simple_plot.R
@@ -1,3 +1,9 @@
 library(palmerpenguins)
 
-plot(penguins$bill_length_mm, penguins$flipper_length_mm)
+plot(
+  penguins$bill_length_mm, 
+  penguins$flipper_length_mm,
+  col = "blue2",
+  pch = 16
+)
+
